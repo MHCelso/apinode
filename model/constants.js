b1bd7319da91d7283c9745531e56b86da638440d
mhcelso <furuyamic@gmail.com>
2019-04-30 14:26:22 -0500

    gitignore carpeta model activada

---
+++ model/constants.js
@@ -0,0 +1,10 @@
+// Objeto que gusrada los datos para la conexion
+const mysqlObject = {
+	host: '187.189.152.4',
+	user: 'xampp',
+	password: 'marquesada?466',
+	database: 'pruebasrecargas',
+	port: 3306
+};
+
+exports.mysqlObject = mysqlObject;
